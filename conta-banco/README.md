68094bde0ac71c55411873dd20e7c8310820dbd5
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-06-29 20:55:46 -0300

    Programa desenvolvido.

--- conta-banco/README.md
+++ conta-banco/README.md
@@ -13,7 +13,7 @@
 | Saldo    | Decimal | 237.48 |
 
 3. Permita que os dados sejam inseridos via terminal sendo que o usuário receberá a mensagem de qual informação será solicitada, exemplo:
-    - Programa: "Por favor, digite o número da Agência !";
+    - Programa: "Por favor, digite o número da Agência: ";
     - Usuário: 1021 (depois ENTER para o próximo campo);
 
 4. Depois de todas as informações terem sido inseridas, o sistema deverá exibir a seguinte mensagem:
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner keyboard = new Scanner(System.in);
         
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencyNumber = keyboard.nextLine();
+        System.out.print("Por favor, digite o número da Conta: ");
+        int accountNumber = keyboard.nextInt();
+        keyboard.nextLine();
+        System.out.print("Por favor, digite o seu nome: ");
+        String accountholder = keyboard.nextLine();
+        System.out.print("Por favor, digite o saldo da conta: ");
+        double balance = keyboard.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", accountholder, agencyNumber, accountNumber, balance);
+
+        keyboard.close();
     }
 }
