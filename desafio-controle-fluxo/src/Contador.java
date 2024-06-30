cef6c3fc1bbee951611665f99e97e482fd664303
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-06-30 17:35:25 -0300

    Implementação do código do desafio

---
+++ desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+
+		System.out.print("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		System.out.print("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.err.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }		
+		int contagem = parametroDois - parametroUm;
+		for(int i = 1; i <= contagem; i++) {
+            System.out.printf("Contagem: %d%n", i);
+        }
+	}
+}
